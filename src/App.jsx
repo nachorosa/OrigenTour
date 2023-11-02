cd53c5790629fece21d5e06127a100e125c022fd
ilrosaunsam <ilrosa@estudiantes.unsam.edu.ar>
2023-11-02 16:38:12 -0300

    agrego componente nav, faltan detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Origen Tour
-    </h1>
-  )
+import React from 'react'
+import Nav from './components/Nav'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav/>
+      </div>
+    );
+  }
 }
+
+export default App
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class WelcomeMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Origen Tour Sitio En Construccion</h1>
+      </div>
+    )
+  }
+}
+
+export default WelcomeMessage
