452efaa56bc46bafb3a193cb5841e57b54fe70e8
ilrosaunsam <ilrosa@estudiantes.unsam.edu.ar>
2023-11-03 17:42:19 -0300

    nav fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,7 +19,7 @@ export default function Nav() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between flex-row-reverse">
+            <div className="relative flex h-24 items-center justify-between flex-row-reverse">
               <div className="inset-y-0 left-0 flex items-center sm:hidden ">
                 <Disclosure.Button className="relative inline-flex items-center justify-between rounded-md p-2 text-orange-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className=" -inset-0.5" />
@@ -30,12 +30,12 @@ export default function Nav() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+              <div className=" flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                 <div className="flex flex-shrink-0 items-center">
-                <img className="h-8 w-auto  hidden sm:block" src="/src/img/logo-desktop.svg" alt="ORIGEN TOUR"/>
+                <img className="h-10 w-auto  hidden sm:block" src="/src/img/logo-desktop.svg" alt="ORIGEN TOUR"/>
                   <div className='sm:hidden flex'>
-                    <img className="h-8" src="/src/img/logo-mobile-1.svg" alt="ORIGEN TOUR"/>
-                    <img className="h-8" src="/src/img/logo-mobile-2.svg" alt="ORIGEN TOUR"/>
+                    <img className="h-10" src="/src/img/logo-mobile-1.svg" alt="ORIGEN TOUR"/>
+                    <img className="h-10" src="/src/img/logo-mobile-2.svg" alt="ORIGEN TOUR"/>
                   </div>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
@@ -45,7 +45,7 @@ export default function Nav() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'text-orange-500 underline' : 'text-orange-800 hover:text-orange-500 hover:underline',
+                          item.current ? 'text-2xl  text-orange-500 underline' : 'text-2xl text-orange-800 hover:text-orange-500 hover:underline',
                           'rounded-md px-3 py-2 text-sm font-medium',
                           item.name === 'Contacto' ? 'bg-orange-500 text-white hover:text-white' : ''
                         )}
