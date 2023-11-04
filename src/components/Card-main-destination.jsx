c8cb9a8f7406b05ca5e7ed2c40135f0ed0c88a5c
ilrosaunsam <ilrosa@estudiantes.unsam.edu.ar>
2023-11-04 17:16:18 -0300

    creo componente destinos principales

---
+++ src/components/Card-main-destination.jsx
@@ -0,0 +1,73 @@
+import "../css/mainDestination.css"
+
+const destinations = [
+    {
+        id: 1,
+        provincia: 'Misisones',
+        lugar: "Cataratas del iguazu",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "cataratas",
+      },
+      {
+        id: 1,
+        provincia: 'pepe',
+        lugar: "Cataratas del iguazu",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "cataratalorem s",
+      },    {
+        id: 1,
+        provincia: 'pepe2',
+        lugar: "Cataratas del iguazu",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "clorem ataratas",
+      },    {
+        id: 1,
+        provincia: 'pepe4',
+        lugar: "Cataratas del iguazu",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "catalorem atas",
+      },    {
+        id: 1,
+        provincia: 'lorem aaa ',
+        lugar: "Cataratas del iguazu",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "catarlorem atas",
+      },    {
+        id: 1,
+        provincia: 'lorem lorem ',
+        lugar: "Clorem ",
+        href: '#',
+        imageSrc: '/src/img/cataratas.jpg',
+        imageAlt: "cataratas",
+      },
+ 
+    // More destinations...
+  ]
+  
+  export const CardMainDestination = () => {
+    return (
+        <div className="container-destination">
+          <h2>Principales Destinos</h2>
+            <div className="container-destination-card">
+                {destinations.map((destiny) => (
+                    <div key={destiny.id} className="card-destination">
+                        <div className="card-destination-detail">
+                            <img src={destiny.imageSrc} alt={destiny.imageAlt}/>
+                            <p className="card-destination-detail-text">{destiny.lugar} </p>
+                            <p className="card-destination-detail-text card-destination-detail-text-province">{destiny.provincia} </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+  }
+  
+
+  
+   
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import { Carousel } from "../components/Carousel"
 import { Promotion } from "../components/Card-Promotion"
+import { CardMainDestination } from "../components/Card-main-destination"
 import { WelcomeMessage } from "../components/WelcomeMessage"
 
 export const Home = () => {
   return (
     <div>
-        <Carousel/>
+        {/* <Carousel/>
         <WelcomeMessage/>
         <div className="flex">
+
         <Promotion/>
-        {/* <Promotion/> */}
-        {/* <Promotion/> */}
-        </div>
+        <Promotion/>
+        <Promotion/>
+        </div> */}
+        <CardMainDestination/>
     </div>
   )
 }
